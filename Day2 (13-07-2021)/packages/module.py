3a964381612d57b5bfcf39bbc4c7be7b6443c8e5
pavanisuvarna27 <51401850+pavanisuvarna27@users.noreply.github.com>
2021-07-13 17:13:29 +0530

    Day2- tasks,pacakges and modules using oop's,intro,project and app creation

---
+++ Day2 (13-07-2021)/packages/module.py
@@ -0,0 +1,10 @@
+class math:
+    def __init__(abc,v1,v2):
+        abc.v1=v1
+        abc.v2=v2
+    def show(abc):
+        print(abc.v1)
+        print(abc.v2)
+    def add(abc,v3):
+        print(abc.v1+abc.v2+v3)
+        
