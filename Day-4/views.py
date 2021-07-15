e10d9b6f0a84c28be2ff74be0e7f672f7ac7e593
srikanya659qwe <51402676+srikanya659qwe@users.noreply.github.com>
2021-07-15 17:19:19 +0530

    Day-4
    Day-4

---
+++ Day-4/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def sample(request):
+	return HttpResponse("hi,welcome to django session")
+
+def home(request):
+	return HttpResponse("<h3><center>hi...guys</center></h3>")
+
+def dynamic(request,id,name):
+	return HttpResponse("my Id is {} and My Name is {}".format(id,name))
+
+def temp(request):
+	return render(request,'myApp/temp.html')
+
+def table(request):
+	return render(request,'myApp/table.html')
+
+def inline(request):
+	return render(request,'myApp/inline.html')
+
+def internal(request):
+	return render(request,'myApp/internal.html')
+
+def external(request):
+	return render(request,'myApp/external.html')
